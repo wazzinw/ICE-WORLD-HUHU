dc056a4ea0f31a680a26f798e083add55e64fc64
armhuhu <marchen-awaken-romance@hotmail.com>
2013-04-17 23:51:10 +0700

    Update NameCheck.java

--- Final_Submission/src/Login/NameCheck.java
+++ Final_Submission/src/Login/NameCheck.java
@@ -1,16 +1,35 @@
 package Login;
 
-public class NameCheck {
-	String name;
-	int cnt;
-	NameCheck(){
-		this.name = "";
-		cnt =0;
-	}
-	NameCheck(String name){
-		this.name = name;
-		cnt =1;
-	}
-	
-	
+public class NameCheck implements Runnable {
+ String name;
+ int cnt;
+ int timer=5;
+ 
+ NameCheck(){
+  this.name = "";
+  cnt =1;
+ }
+ NameCheck(String name){
+  this.name = name;
+  cnt =1;
+ }
+ @Override
+ public void run() {
+  if(cnt>=3){
+   for(int i =0;i<5;i++){
+    try {
+     Thread.sleep(60000);
+    } catch (InterruptedException e) {
+     // TODO Auto-generated catch block
+     e.printStackTrace();
+    }
+    timer--;
+   }
+   cnt = 0;
+   timer =5;
+  }
+  
+ }
+ 
+ 
 }
